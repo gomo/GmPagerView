234c059994ce48b198eb4743244da646cd1663e8
Masamoto Miyata <miyata@sincere-co.com>
2013-01-07 00:47:09 +0900

    Initial Commit

---
+++ GmPagerView/GmAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GmAppDelegate.h
+//  GmPagerView
+//
+//  Created by Masamoto Miyata on 2013/01/07.
+//  Copyright (c) 2013年 Masamoto Miyata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GmAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GmPagerView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GmPagerView
+//
+//  Created by Masamoto Miyata on 2013/01/07.
+//  Copyright (c) 2013年 Masamoto Miyata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GmAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GmAppDelegate class]));
+    }
+}
